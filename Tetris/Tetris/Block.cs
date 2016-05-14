4152c5ab1c228b21c77a94486cefd62c5ddad223
iamaudrey <iamaudrey@eagles.ewu.edu>
2016-05-13 23:41:24 -0700

    Added Block and cell classes

---
+++ Tetris/Tetris/Block.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Block
+    {
+        private Cell[][] cells;
+        public Cell[][] Cells
+        {
+            get
+            {
+                return cells;
+            }
+            set
+            {
+                if (value != null)
+                    cells = value;
+            }
+        }
+
+        private String color;
+        public String Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                if (value != null)
+                    color = value;
+            }
+        }
+
+        private int[] originCoords;
+        public int[] OriginCoords
+        {
+            get
+            {
+                return originCoords;
+            }
+            set
+            {
+                originCoords = value;
+            }
+        }
+        
+        //Constructor
+        public Block()
+        {
+
+        }
+    }
+
+    class Cell
+    {
+        private bool isPopulated;
+        public bool IsPopulated { get; set; }
+
+        //Constructors
+        public Cell()
+        {
+            isPopulated = true;
+        }
+        public Cell(bool populated)
+        {
+            isPopulated = populated;
+        }
+    }
+}
